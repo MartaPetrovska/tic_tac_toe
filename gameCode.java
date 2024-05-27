9e756f1404eea11d6e53848a46a31fd97ce83839
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-05-27 18:37:21 +0300

    Update gameCode.java

--- gameCode.java
+++ gameCode.java
@@ -2,42 +2,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      boolean gameContinues = true;
       int grid_size = 3;
       int[][] grid = new int[grid_size][grid_size];
       int userSymbol = 1;
       int computerSymbol = 2;
+      boolean gameIsOn = true;
         
-      while (gameContinues) {
+      //Games main loop
+      while (gameIsOn) {
+
+        //Code for users turn
         userTurn(grid);
-        printArray(grid, grid_size);
-        if (checkLines(grid, userSymbol)) {
+        printGrid(grid, grid_size);
+        if (checkWin(grid, userSymbol)) {
           System.out.println("User wins!");
-          gameContinues = false;
+          gameIsOn = false;
+          break;
+        } 
+
+        //Code for computers turn
+        computerTurn(grid);
+        System.out.println("Oponents turn:");
+        printGrid(grid, grid_size);
+        if (checkWin(grid, computerSymbol)) {
+          System.out.println("Computer wins!");
+          gameIsOn = false;
           break;
         }
 
-        computerTurn(grid);
-        System.out.println("Oponents turn:");
-        printArray(grid, grid_size);
-        if (checkLines(grid, computerSymbol)) {
-          System.out.println("Computer wins!");
-          gameContinues = false;
+        //Code to check if grid is full and if there is a draw.
+        if (checkDraw(grid, grid_size)) {
+          System.out.println("It's a draw!");
+          gameIsOn = false;
           break;
         }
       }
     }
 
+    // Function to handle users turn.
     public static void userTurn(int[][] grid) {
-      
       Scanner scanner = new Scanner(System.in);
       boolean emptyField = true;
-      
       while (emptyField) {
-        System.out.println("Your turn. Write row number (0/1/2):");
-        int row = scanner.nextInt();
-        System.out.println("Write column number (0/1/2):");
-        int column = scanner.nextInt();
+        System.out.println("Your turn. Write row number (1/2/3):");
+        int row = scanner.nextInt() - 1;
+        System.out.println("Write column number (1/2/3):");
+        int column = scanner.nextInt() - 1;
           if (grid[row][column] == 0) {
             grid[row][column] = 1;
             emptyField = false;
@@ -47,10 +57,9 @@ public class Main {
       }
     }
 
+    // Function to handle computers turn. 
     public static void computerTurn(int[][] grid) {
-  
       boolean emptyField = true;
-      
       while (emptyField) {
         int row = (int)(Math.random() * 3);
         int column = (int)(Math.random() * 3);
@@ -61,7 +70,8 @@ public class Main {
       }
     }
 
-    public static void printArray(int[][] grid, int grid_size) {
+    // Function that print out grid. 
+    public static void printGrid(int[][] grid, int grid_size) {
       for (int i = 0; i < grid_size; i++) {
           for (int j = 0; j < grid_size; j++) {
             System.out.print(grid[i][j] + " ");
@@ -70,21 +80,20 @@ public class Main {
       }
     }
 
-    public static boolean checkLines(int[][] grid, int symbol) {
+    // Function that checks each line if there is a win. 
+    public static boolean checkWin(int[][] grid, int symbol) {
       // Check rows
       for (int i = 0; i < grid.length; i++) {
         if (grid[i][0] != 0 && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
           return true;
         }
       }
-
       // Check columns
       for (int i = 0; i < grid.length; i++) {
         if (grid[0][i] != 0 && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
           return true;
         }
       }
-
       // Check diagonals
       if (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) {
         return true;
@@ -94,5 +103,18 @@ public class Main {
       };
 
       return false;
+    }
+
+    // Function that checks if grid is full and if there is a draw. 
+    public static boolean checkDraw(int[][] grid, int grid_size) {
+      int filledFields = 0;
+      for (int i = 0; i < grid_size; i++) {
+          for (int j = 0; j < grid_size; j++) {
+            if (grid[i][j] != 0) {
+              filledFields ++;
+            }
+          }
+      }
+      return filledFields == grid_size * grid_size;
     }
 }

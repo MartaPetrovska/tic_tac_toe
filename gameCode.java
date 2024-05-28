410ec08056624d001fb489b883361d9521e72f10
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-05-28 19:29:26 +0300

    Update gameCode.java

--- gameCode.java
+++ gameCode.java
@@ -7,69 +7,116 @@ public class Main {
       int userSymbol = 1;
       int computerSymbol = 2;
       boolean gameIsOn = true;
-        
+      Scanner scanner = new Scanner(System.in);
+
+      // Initializing game. 
+      System.out.println("Welcome to tic-tac-toe game! Here is games field.");
+      printGrid(grid, grid_size);
+      System.out.println ("When it is your turn write row and column coordinates to place your symbol..");
+      System.out.println();
+
+      // Choosing which goes first (user or computer)
+      int firstTurn = (int)(Math.random() * 2);
+      if (firstTurn == 0) {
+        System.out.println("You start the game");
+      }
+      if (firstTurn == 1) {
+        System.out.println("Opponent starts the game.");
+      }
+      
       //Games main loop
       while (gameIsOn) {
+        // If user goes first
+        if (firstTurn == 0){
+          // Users turn
+          userTurn(grid, scanner);
+          printGrid(grid, grid_size);
+          System.out.println();
+          if (checkWin(grid, userSymbol)) {
+            System.out.println("Congratulation! You win!");
+            gameIsOn = false;
+            break;
+          } 
 
-        //Code for users turn
-        userTurn(grid);
-        printGrid(grid, grid_size);
-        if (checkWin(grid, userSymbol)) {
-          System.out.println("User wins!");
-          gameIsOn = false;
-          break;
-        } 
+          // Computers turn
+          computerTurn(grid);
+          System.out.println("Oponents turn:");
+          printGrid(grid, grid_size);
+          System.out.println();
+          if (checkWin(grid, computerSymbol)) {
+            System.out.println("Computer wins!");
+            gameIsOn = false;
+            break;
+          }
 
-        //Code for computers turn
-        computerTurn(grid);
-        System.out.println("Oponents turn:");
-        printGrid(grid, grid_size);
-        if (checkWin(grid, computerSymbol)) {
-          System.out.println("Computer wins!");
-          gameIsOn = false;
-          break;
+        } else { 
+          // Computers turn
+          computerTurn(grid);
+          System.out.println("Oponents turn:");
+          printGrid(grid, grid_size);
+          System.out.println();
+          if (checkWin(grid, computerSymbol)) {
+            System.out.println("Computer wins!");
+            gameIsOn = false;
+            break;
+          }
+
+          // Users turn
+          userTurn(grid, scanner);
+          printGrid(grid, grid_size);
+          System.out.println();
+          if (checkWin(grid, userSymbol)) {
+            System.out.println("Congratulation! You win!");
+            gameIsOn = false;
+            break;
+          } 
         }
 
-        //Code to check if grid is full and if there is a draw.
+        //Check for draw.
         if (checkDraw(grid, grid_size)) {
           System.out.println("It's a draw!");
           gameIsOn = false;
           break;
         }
       }
+      scanner.close();
     }
 
+  
     // Function to handle users turn.
-    public static void userTurn(int[][] grid) {
-      Scanner scanner = new Scanner(System.in);
+    public static void userTurn(int[][] grid, Scanner scanner) {
       boolean emptyField = true;
       while (emptyField) {
         System.out.println("Your turn. Write row number (1/2/3):");
         int row = scanner.nextInt() - 1;
         System.out.println("Write column number (1/2/3):");
         int column = scanner.nextInt() - 1;
-          if (grid[row][column] == 0) {
-            grid[row][column] = 1;
-            emptyField = false;
-          } else {
-            System.out.println("Field is already taken. Please choose another one.");
-          }
+        
+        if (grid[row][column] == 0) {
+          grid[row][column] = 1;
+          emptyField = false;
+        } else {
+          System.out.println("Field is already taken. Please choose another one.");
+        }
       }
     }
 
+  
     // Function to handle computers turn. 
     public static void computerTurn(int[][] grid) {
       boolean emptyField = true;
       while (emptyField) {
         int row = (int)(Math.random() * 3);
         int column = (int)(Math.random() * 3);
-          if (grid[row][column] == 0) {
-            grid[row][column] = 2;
-            emptyField = false;
-          } 
+        
+        if (grid[row][column] == 0) {
+          grid[row][column] = 2;
+          emptyField = false;
+        } 
       }
     }
 
+  
     // Function that print out grid. 
     public static void printGrid(int[][] grid, int grid_size) {
       for (int i = 0; i < grid_size; i++) {
@@ -80,6 +127,7 @@ public class Main {
       }
     }
 
+  
     // Function that checks each line if there is a win. 
     public static boolean checkWin(int[][] grid, int symbol) {
       // Check rows
@@ -105,7 +153,8 @@ public class Main {
       return false;
     }
 
-    // Function that checks if grid is full and if there is a draw. 
+  
+    // Function that checks if grid is full and there is a draw. 
     public static boolean checkDraw(int[][] grid, int grid_size) {
       int filledFields = 0;
       for (int i = 0; i < grid_size; i++) {
@@ -117,4 +166,5 @@ public class Main {
       }
       return filledFields == grid_size * grid_size;
     }
+  
 }
